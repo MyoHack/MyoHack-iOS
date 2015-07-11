a29ab88f137ad428556f24b3c5667321575aaad9
Jay Ravaliya <jayrav13@gmail.com>
2015-07-11 14:54:28 -0400

    Line graph

--- MyoHackProject/ViewController.swift
+++ MyoHackProject/ViewController.swift
@@ -9,21 +9,38 @@
 import UIKit
 
 var myoIdentifier : NSUUID = NSUUID(UUIDString: "0A66A6EA-D5D1-F872-E701-49AD2FFC45E4")!
+var xPoints : [CGFloat] = [0.0]
+var yPoints : [CGFloat] = [0.0]
+var zPoints : [CGFloat] = [0.0]
 
 class ViewController: UIViewController {
 
+    var lineChart: LineChart!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         TLMHub.sharedHub().attachByIdentifier(myoIdentifier)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "didReceiveOrientationEvent:", name: TLMMyoDidReceiveOrientationEventNotification, object: nil)
+    
+        xPoints = [1,2,3,4,5]
+        
+        // line chart
+        lineChart = LineChart(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/2))
+        lineChart.area = true
+        lineChart.animation.enabled = false
+        lineChart.x.grid.count = 1
+        lineChart.y.grid.count = 1
+        lineChart.addLine(xPoints)
+        view.addSubview(lineChart)
+        
     }
     
     func didReceiveOrientationEvent(notification: NSNotification) {
         var orientation = notification.userInfo![kTLMKeyOrientationEvent]! as! TLMOrientationEvent
         
+        refreshGraph(orientation.quaternion)
         
         println("\(orientation.quaternion.x), \(orientation.quaternion.y), \(orientation.quaternion.z), \(orientation.quaternion.w)")
     }
@@ -45,5 +62,17 @@ class ViewController: UIViewController {
         }
     }
 
+    func refreshGraph(quat : TLMQuaternion)
+    {
+        lineChart.clearAll()
+        lineChart.removeFromSuperview()
+        if xPoints.count > 20
+        {
+            xPoints.removeAtIndex(0)
+        }
+        xPoints.append(CGFloat(quat.x))
+        
+        view.addSubview(lineChart)
+    }
 
 }
