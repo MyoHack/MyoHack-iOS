53197c6c3cf9f83e9c05a8d2de9378a6878bc388
Ismail Mustafa <ismailmustafa@rocketmail.com>
2015-07-11 17:24:59 -0400

    fucking baller

--- MyoHackProject/ViewController.swift
+++ MyoHackProject/ViewController.swift
@@ -12,16 +12,16 @@ var myoIdentifier : NSUUID = NSUUID(UUIDString: "0A66A6EA-D5D1-F872-E701-49AD2FF
 var xPoints : [CGFloat] = [0.0]
 var yPoints : [CGFloat] = [0.0]
 var zPoints : [CGFloat] = [0.0]
+var wPoints : [CGFloat] = [0.0]
 
-var temp : [CGFloat] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+var temp : [CGFloat] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 class ViewController: UIViewController, BEMSimpleLineGraphDataSource, BEMSimpleLineGraphDelegate {
 
     var xGraph : BEMSimpleLineGraphView!
     var yGraph : BEMSimpleLineGraphView!
     var zGraph : BEMSimpleLineGraphView!
-    
-    var counter = 1
+    var wGraph : BEMSimpleLineGraphView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,15 +29,17 @@ class ViewController: UIViewController, BEMSimpleLineGraphDataSource, BEMSimpleL
         xPoints = temp
         yPoints = temp
         zPoints = temp
+        wPoints = temp
         
         TLMHub.sharedHub().attachByIdentifier(myoIdentifier)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "didReceiveOrientationEvent:", name: TLMMyoDidReceiveOrientationEventNotification, object: nil)
         
         
-        xGraph = BEMSimpleLineGraphView(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/3))
-        yGraph = BEMSimpleLineGraphView(frame: CGRectMake(self.view.frame.size.height/3, 0, self.view.frame.size.width, self.view.frame.size.height/3))
-        zGraph = BEMSimpleLineGraphView(frame: CGRectMake((self.view.frame.size.height*2)/3, 0, self.view.frame.size.width, self.view.frame.size.height/3))
+        xGraph = BEMSimpleLineGraphView(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/4))
+        yGraph = BEMSimpleLineGraphView(frame: CGRectMake(0, self.view.frame.size.height/4, self.view.frame.size.width, self.view.frame.size.height/4))
+        zGraph = BEMSimpleLineGraphView(frame: CGRectMake(0, (self.view.frame.size.height*2)/4, self.view.frame.size.width, self.view.frame.size.height/4))
+        wGraph = BEMSimpleLineGraphView(frame: CGRectMake(0, (self.view.frame.size.height*3)/4, self.view.frame.size.width, self.view.frame.size.height/4))
         
         xGraph.animationGraphStyle = BEMLineAnimation.None
         xGraph.enableBezierCurve = true
@@ -57,15 +59,17 @@ class ViewController: UIViewController, BEMSimpleLineGraphDataSource, BEMSimpleL
         zGraph.delegate = self
         self.view.addSubview(zGraph)
         
-        
+        wGraph.animationGraphStyle = BEMLineAnimation.None
+        wGraph.enableBezierCurve = true
+        wGraph.dataSource = self
+        wGraph.delegate = self
+        self.view.addSubview(wGraph)
     }
     
     func didReceiveOrientationEvent(notification: NSNotification) {
         var orientation = notification.userInfo![kTLMKeyOrientationEvent]! as! TLMOrientationEvent
         
-        xPoints.append(CGFloat(orientation.quaternion.x))
-        yPoints.append(CGFloat(orientation.quaternion.y))
-        zPoints.append(CGFloat(orientation.quaternion.z))
+        
         
         self.refreshGraph(orientation.quaternion)
         
@@ -73,59 +77,46 @@ class ViewController: UIViewController, BEMSimpleLineGraphDataSource, BEMSimpleL
     }
     
     func refreshGraph(quat : TLMQuaternion) {
-    
-        /*
-        if (counter % 10 == 0) {
-            if xPoints.count <= 20 {
-                xPoints.append(CGFloat(quat.x))
-                xGraph.reloadGraph()
-            }
-            else {
-                xPoints.removeAtIndex(0)
-                xPoints.append(CGFloat(quat.x))
-                xGraph.reloadGraph()
-            }
-            counter = 0
-        }
-        counter++
-*/
+        
+        xPoints.append(CGFloat(quat.x))
+        yPoints.append(CGFloat(quat.y))
+        zPoints.append(CGFloat(quat.z))
+        wPoints.append(CGFloat(quat.w))
+        
         xGraph.reloadGraph()
+        yGraph.reloadGraph()
+        zGraph.reloadGraph()
+        wGraph.reloadGraph()
     }
     
     func numberOfPointsInLineGraph(graph: BEMSimpleLineGraphView) -> Int {
         return 20
     }
     
+    func minValueForLineGraph(graph: BEMSimpleLineGraphView) -> CGFloat {
+        return -1.0
+    }
+    
+    func maxValueForLineGraph(graph: BEMSimpleLineGraphView) -> CGFloat {
+        return 1.0
+    }
+    
     func lineGraph(graph: BEMSimpleLineGraphView, valueForPointAtIndex index: Int) -> CGFloat {
-        if graph == xGraph
-        {
+        if graph == xGraph {
             return xPoints[xPoints.count - 20 + index]
         }
-        else if graph == yGraph
-        {
+        else if graph == yGraph {
             return yPoints[yPoints.count - 20 + index]
         }
-        else
-        {
-            return zPoints[yPoints.count - 20 + index]
+        else if graph == wGraph {
+            return zPoints[zPoints.count - 20 + index]
+        }
+        else {
+            return wPoints[wPoints.count - 20 + index]
         }
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        //self.modalPresentMyoSettings()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func modalPresentMyoSettings() {
-        var settings : UINavigationController = TLMSettingsViewController.settingsInNavigationController()
-        self.presentViewController(settings, animated: true) { () -> Void in
-            
-        }
-    }
-
 }
