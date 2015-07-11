322e0c9e284eccf7b7434ba28cdc806cbc7a72cb
Ismail Mustafa <ismailmustafa@rocketmail.com>
2015-07-11 14:13:47 -0400

    auto connecting to myo band

--- MyoHackProject/ViewController.swift
+++ MyoHackProject/ViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 
+var myoIdentifier : NSUUID = NSUUID(UUIDString: "0A66A6EA-D5D1-F872-E701-49AD2FFC45E4")!
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        TLMHub.sharedHub().attachByIdentifier(myoIdentifier)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "didReceiveOrientationEvent:", name: TLMMyoDidReceiveOrientationEventNotification, object: nil)
+    }
+    
+    func didReceiveOrientationEvent(notification: NSNotification) {
+        var orientation = notification.userInfo![kTLMKeyOrientationEvent]! as! TLMOrientationEvent
+        
+        
+        println("\(orientation.quaternion.x), \(orientation.quaternion.y), \(orientation.quaternion.z), \(orientation.quaternion.w)")
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        self.modalPresentMyoSettings()
+        //self.modalPresentMyoSettings()
     }
 
     override func didReceiveMemoryWarning() {
